af6f4f7cc315561dc3dbd2b1e4315049234e08a3
mirza-logics <131347211+mirza-logics@users.noreply.github.com>
2023-08-05 18:54:22 +0500

    webrtc

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('hi')
+  res.redirect(`/${uuidV4()}`)
 })
 
-app.listen(5000)
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+    console.log('join')
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(2000)
